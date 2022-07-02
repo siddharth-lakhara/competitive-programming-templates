84645e06c3a2de3d35e2776ea41fcb70c5fbd615
lakhara <siddharth.lakhara@gmail.com>
2022-07-02 20:59:59 +0530

    binary search tree

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,22 @@
-import algo.search.BinarySearch;
+import ds.Tree.BinarySearchTree;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] sortedArray = new int[]{1,2,3,4,5,6,7,9, 10};
-        int foundAtIdx = BinarySearch.search(sortedArray, 10);
-        System.out.println("Found: " + foundAtIdx);
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.addElement(5);
+        bst.addElement(3);
+        bst.addElement(2);
+        bst.addElement(4);
+        bst.addElement(9);
+        bst.addElement(7);
+        bst.addElement(11);
+
+//        System.out.println(bst.search(7));
+
+//        int lca = bst.lowestCommonAncestor(7, 11);
+//        System.out.println("Lowest Common Ancestor: " + lca);
+
+        bst.floorAndCeil(100);
     }
 }
--- src/ds/Tree/BinarySearchTree.java
+++ src/ds/Tree/BinarySearchTree.java
@@ -1,5 +1,9 @@
 package ds.Tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class BinarySearchTree {
 
     private TreeNode head;
@@ -42,6 +46,8 @@ public class BinarySearchTree {
         this.printInOrder(node.right);
     }
 
+//    Searches a node. Returns boolean based on whether node found
+//    Uses DFS
     public boolean search(int data) {
         if (this.head == null) {
             return false;
@@ -63,6 +69,110 @@ public class BinarySearchTree {
             return search(node.left, data);
         }
     }
+
+    public void printAncestors(int data) {
+        Stack<Integer> st = new Stack<>();
+
+        if (this.head == null) {
+            System.out.println("Tree is empty");
+            return;
+        }
+
+        this.printAncestors(this.head, data, st);
+    }
+
+    private void printAncestors(TreeNode node, int data, Stack<Integer> st) {
+        if (node == null) {
+            System.out.println("Node not found");
+            return;
+        }
+        if (node.data == data) {
+            System.out.println("Printing ancestors: ");
+            System.out.println(st.toString());
+            return ;
+        }
+
+        st.push(node.data);
+        if (data > node.data) {
+            printAncestors(node.right, data, st);
+        } else {
+            printAncestors(node.left, data, st);
+        }
+
+    }
+
+//    Print the tree level by level
+    public void breadthFirstSearch() {
+        Queue<TreeNode> levelQueue = new LinkedList<>();
+        levelQueue.add(this.head);
+        this.levelPrint(levelQueue, 0);
+    }
+
+    private void levelPrint(Queue<TreeNode> levelQueue, int level) {
+        int len = levelQueue.size();
+        if (len == 0) {
+            return ;
+        }
+
+        String levelAsString = "";
+        while (len > 0) {
+            TreeNode node = levelQueue.poll();
+            levelAsString += node.data + " ";
+            if (node.left != null) {
+                levelQueue.add(node.left);
+            }
+            if (node.right != null) {
+                levelQueue.add(node.right);
+            }
+            len--;
+        }
+        System.out.println("Level " + level + ": " + levelAsString);
+
+        levelPrint(levelQueue, level++);
+    }
+
+//    assumes both nodes are present in tree
+    public int lowestCommonAncestor(int dataA, int dataB) {
+        return lowestCommonAncestor(this.head, dataA, dataB);
+    };
+
+    private int lowestCommonAncestor(TreeNode currNode, int dataA, int dataB) {
+        if (currNode.data == dataA || currNode.data == dataB) {
+            return currNode.data;
+        }
+        if (dataA > currNode.data && dataB > currNode.data) {
+            return lowestCommonAncestor(currNode.right, dataA, dataB);
+        }
+        if (dataA < currNode.data && dataB < currNode.data) {
+            return lowestCommonAncestor(currNode.left, dataA, dataB);
+        }
+        return currNode.data;
+    }
+
+    public void floorAndCeil(int data) {
+        this.floorAndCeil(this.head, data, -1, -1);
+    }
+
+    private void floorAndCeil(TreeNode node, int data, int floor, int ceil) {
+        if (node == null) {
+            System.out.printf("Floor: %d, Ceil: %d", floor, ceil);
+            return;
+        }
+
+        if (data == node.data) {
+            floor = ceil = node.data;
+            System.out.printf("Floor: %d, Ceil: %d", floor, ceil);
+            return;
+        }
+
+        if (data > node.data) {
+            floor = floor == -1 ? node.data : Math.max(node.data, floor);
+            this.floorAndCeil(node.right, data, floor, ceil);
+        } else {
+            ceil = ceil == -1 ? node.data : Math.min(node.data, ceil);
+            this.floorAndCeil(node.left, data, floor, ceil);
+        }
+    }
 //    ***
 //    Driver code
 //    ***
@@ -70,9 +180,18 @@ public class BinarySearchTree {
 //    public static void main(String[] args) {
 //        BinarySearchTree bst = new BinarySearchTree();
 //        bst.addElement(5);
+//        bst.addElement(3);
 //        bst.addElement(2);
+//        bst.addElement(4);
+//        bst.addElement(9);
 //        bst.addElement(7);
-//
-//        System.out.println(bst.search(7));
+//        bst.addElement(11);
+
+// //       System.out.println(bst.search(7));
+
+// //       int lca = bst.lowestCommonAncestor(7, 11);
+// //       System.out.println("Lowest Common Ancestor: " + lca);
+
+//        bst.floorAndCeil(100);
 //    }
 }
