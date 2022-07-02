ce79c560d9ecd544a9710c54e6c2efa18a5a24c0
lakhara <siddharth.lakhara@gmail.com>
2022-07-02 11:19:30 +0530

    binary search tree

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         BinarySearchTree bst = new BinarySearchTree();
+        bst.addElement(5);
+        bst.addElement(2);
+        bst.addElement(7);
+
+        System.out.println(bst.search(7));
     }
 }
--- src/ds/Tree/BinarySearchTree.java
+++ src/ds/Tree/BinarySearchTree.java
@@ -1,4 +1,78 @@
 package ds.Tree;
 
 public class BinarySearchTree {
+
+    private TreeNode head;
+
+    public BinarySearchTree() {
+        this.head = null;
+    }
+
+    public void addElement(int data) {
+        this.head = this.append(this.head, data);
+    }
+
+    private TreeNode append(TreeNode node, int data) {
+        if (node == null) {
+            node = new TreeNode(data);
+            return node;
+        }
+
+        if (node.data > data) {
+            node.left = this.append(node.left, data);
+        } else {
+            node.right = this.append(node.right, data);
+        }
+
+        return node;
+    }
+
+    public void printInOrder() {
+        this.printInOrder(this.head.left);
+        System.out.println(this.head.data + " ");
+        this.printInOrder(this.head.right);
+    }
+
+    private void printInOrder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        this.printInOrder(node.left);
+        System.out.println(node.data + " ");
+        this.printInOrder(node.right);
+    }
+
+    public boolean search(int data) {
+        if (this.head == null) {
+            return false;
+        }
+        return this.search(this.head, data);
+    }
+
+    private boolean search(TreeNode node, int data) {
+        if (node == null) {
+            return false;
+        }
+        if (node.data == data) {
+            return true;
+        }
+
+        if (data > node.data) {
+            return search(node.right, data);
+        } else {
+            return search(node.left, data);
+        }
+    }
+//    ***
+//    Driver code
+//    ***
+//
+//    public static void main(String[] args) {
+//        BinarySearchTree bst = new BinarySearchTree();
+//        bst.addElement(5);
+//        bst.addElement(2);
+//        bst.addElement(7);
+//
+//        System.out.println(bst.search(7));
+//    }
 }
